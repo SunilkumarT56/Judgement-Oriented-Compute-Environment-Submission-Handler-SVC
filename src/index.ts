2200ab986c0dca315c91b22214d975d384b34cbd
Sunilkumar <sunilcse2006@gmail.com>
2025-12-01 20:21:40 +0530

    init a svc

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const app = express();
 app.get("/me", (req, res) => {
     res.send("Hello World!");
 });
+app.get("/health", (req, res) => {
+    res.send("OK");
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
